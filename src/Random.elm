0da309c56af2ec1a49805f5e75ae7d3c452e38de
takayukioda <takayuki@odashi.me>
2018-09-30 15:21:00 +0900

    feat: deal with random
    <https://guide.elm-lang.org/effects/random.html>

---
+++ src/Random.elm
@@ -0,0 +1,72 @@
+module Main exposing (..)
+
+import Browser
+import Html exposing (..)
+import Html.Events exposing (..)
+import Random
+
+
+
+-- Main
+
+
+main =
+    Browser.element
+    { init = init
+    , update = update
+    , subscriptions = subscriptions
+    , view = view
+    }
+
+
+
+-- Model
+
+
+type alias Model =
+    { dieFace : Int
+    }
+
+
+init : () -> (Model, Cmd Msg)
+init _ =
+    ( Model 1
+    , Cmd.none
+    )
+
+
+
+-- Update
+
+
+type Msg
+    = Roll
+    | NewFace Int
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+    case msg of
+        Roll ->
+            ( model
+            , Random.generage NewFace (Random.int 1 6)
+            )
+        NewFace newFace ->
+            ( Model newFace
+            , Cmd.none
+            )
+
+
+
+-- Subscriptions
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ h1 [] [ text (String.fromInt model.dieFace) ]
+        , button [ onClick Roll ] [ text "Roll" ]
+        ]
